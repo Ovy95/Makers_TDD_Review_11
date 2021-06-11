84422612017567c6574b57095d00ad9de984786a
Ovy95 <jackoverton21@hotmail.com>
2021-06-11 09:33:14 +0100

    Passing tests added

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -1,13 +1,17 @@
-describe("ClassName", function() {
+describe("Blunt", function() {
   var blunt;
 
   beforeEach (function () {
     blunt = new Blunt();
   });
 
-  describe('functionName', function() {
-      it ("testing Hardcoded",function() {
+  describe('Adding tests ', function() {
+      it ("1+1 returns ['1 + 1', 2]",function() {
         expect(blunt.Calculator("1 + 1")).toEqual(["1 + 1", 2]);
+      })
+
+      it ("2+2 returns ['2 + 2', 4]",function() {
+        expect(blunt.Calculator("2 + 2")).toEqual(["2 + 2", 4]);
       })
       
   })
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,8 +1,16 @@
 class Blunt {
 
   Calculator(sum){
+    // "1 + 1"
+    let total  
+    let sumArray= sum.split(" ")
+    if(sumArray[1]== "+"){
+     total = parseInt(sumArray[0]) + parseInt(sumArray[2])
+     return [sum ,total]
 
-  return ["1 + 1", 2]
+    }
+
+
   }
 
 }
